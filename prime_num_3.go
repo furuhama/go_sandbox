d8414d8ab76f7020036425c313437b4fd8bfb16f
furuhama <furuhama@users.noreply.github.com>
2017-11-14 13:59:35 +0900

    formatting prime_num_3.go

--- prime_num_3.go
+++ prime_num_3.go
@@ -9,18 +9,18 @@ import (
 )
 
 func main() {
-	primes := make([]int64, 1) // list of prime number
+	primes := make([]int64, 1)     // list of prime number
 	primes_f := make([]float64, 1) // list of prime number (cast to float)
-	primes[0] = 2 // 2 is a prime number
-	primes_f[0] = 2.0 // 2 is a prime number (float)
+	primes[0] = 2                  // 2 is a prime number
+	primes_f[0] = 2.0              // 2 is a prime number (float)
 	var max int64 = 100
 
 	start := time.Now() // measure time from here
 	var n int64 = 3
 	for n = 3; n < max; n += 2 { // check only odd number from 3
 		flag := true
-		f := float64(n) // casting n to float
-		rf := math.Sqrt(f) // get √n
+		f := float64(n)                    // casting n to float
+		rf := math.Sqrt(f)                 // get √n
 		for i := 1; i < len(primes); i++ { // check by already known prime number greater than 2
 			if primes_f[i] > rf { // just check numbers smaller than √n
 				break
@@ -30,7 +30,7 @@ func main() {
 			}
 		}
 		if flag {
-			primes = append(primes, n) // add prime number
+			primes = append(primes, n)     // add prime number
 			primes_f = append(primes_f, f) // add prime number(float)
 		}
 	}
