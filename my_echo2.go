3743df24aa20cb393bb28571927b7f3a2f519252
furuhama <furuhama@users.noreply.github.com>
2017-11-14 17:32:07 +0900

    add pattern2 to my_echo2.go

--- my_echo2.go
+++ my_echo2.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	s, sep := "", ""
-	for _, arg := range os.Args[1:] { // range generates index, value
-		s += sep + arg
-		sep = " "
-	}
-	fmt.Println(s)
+	// Patten 1
+	//
+	//s, sep := "", ""
+	//for _, arg := range os.Args[1:] { // range generates index, value
+	//	s += sep + arg
+	//	sep = " "
+	//}
+	//fmt.Println(s)
+
+	// Pattern 2
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
