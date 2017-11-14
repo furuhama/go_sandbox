c9bc66588c43798790f65c30b69108b85a2a44eb
furuhama <furuhama@users.noreply.github.com>
2017-11-14 18:09:28 +0900

    compare the time of two pattern

--- my_echo2.go
+++ my_echo2.go
@@ -3,25 +3,38 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
+	fmt.Println("Pattern1 start...")
+	start1 := time.Now()
 	// Patten 1
-	//
-	//s, sep := "", ""
-	//for _, arg := range os.Args[1:] { // range generates index, value
-	//	s += sep + arg
-	//	sep = " "
-	//}
-	//fmt.Println(s)
+	// using range
+	s, sep := "", ""
+	for _, arg := range os.Args[1:] { // range generates index, value
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+	goal1 := time.Now()
 
+	fmt.Println("Pattern2 start...")
+	start2 := time.Now()
 	// Pattern 2
 	// this returns process name & result
-	fmt.Printf("Process name:\n%v\n\n", os.Args[0])
-	//fmt.Printf("Result:\n%v", strings.Join(os.Args[1:], " "))
-	fmt.Printf("Result: \n")
+	//fmt.Printf("Process name:\n%v\n\n", os.Args[0])
+	//fmt.Printf("Result:\n")
+	fmt.Printf(strings.Join(os.Args[1:], " "))
+	goal2 := time.Now()
 
-	for key, val := range os.Args[1:] {
-		fmt.Printf("%v: %v", key, val)
-	}
+	// Pattern 3
+	// index, value
+	//for key, val := range os.Args[1:] {
+	//	fmt.Printf("%v: %v", key, val)
+	//}
+
+	// measure time
+	fmt.Printf("\nPattern1:\n%v\nPattern2:\n%v", goal1.Sub(start1), goal2.Sub(start2))
 }
