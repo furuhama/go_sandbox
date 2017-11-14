f6a0399fa54beb109c625cffbc2b568ced8880c4
furuhama <furuhama@users.noreply.github.com>
2017-11-14 17:44:55 +0900

    display process name in my_echo2

--- my_echo2.go
+++ my_echo2.go
@@ -17,5 +17,7 @@ func main() {
 	//fmt.Println(s)
 
 	// Pattern 2
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	// this returns process name & result
+	fmt.Printf("Process name:\n%v\n\n", os.Args[0])
+	fmt.Printf("Result:\n%v", strings.Join(os.Args[1:], " "))
 }
