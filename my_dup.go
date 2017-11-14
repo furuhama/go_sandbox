d9073eae06340c02f42c4c1680b1203563287ca0
furuhama <furuhama@users.noreply.github.com>
2017-11-14 20:17:24 +0900

    make my_dup.go

---
+++ my_dup.go
@@ -0,0 +1,22 @@
+// this returns lines which appears twice or more in stdout
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// [caution] ignore the possibility of error from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
