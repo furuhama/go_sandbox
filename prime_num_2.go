efadcfe6e79f23856c6d4cbfbbd1271183944d23
furuhama <furuhama@users.noreply.github.com>
2017-11-14 10:54:48 +0900

    make prime_num_2.go (use Sieve of Eratosthenes)

---
+++ prime_num_2.go
@@ -0,0 +1,34 @@
+// use Sieve of Eratosthenes
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	max := 100                        // set max number
+	primes := make([]int64, 0)        // list of prime numbers
+	isNotPrime := make([]bool, max+1) // initialize as false(= prime number)
+	isNotPrime[0] = true              // 0 is not prime number
+	isNotPrime[1] = true              // 1 is not prime number
+
+	start := time.Now() // measure time from here
+	n := 2
+	for ; n*n <= max; n++ { // put into sieve at most sqrl(max)
+		if !isNotPrime[n] {
+			primes = append(primes, int64(n)) // add prime number
+			for m := 2; n*m <= max; m++ {
+				isNotPrime[n*m] = true // set not prime number
+			}
+		}
+	}
+	for ; n <= max; n++ {
+		if !isNotPrime[n] {
+			primes = append(primes, int64(n)) // add prime number
+		}
+	}
+	goal := time.Now() // measure time end
+	fmt.Printf("prime numbers under %v are %v\n", max, primes)
+	fmt.Printf("time: %v", goal.Sub(start))
+}
