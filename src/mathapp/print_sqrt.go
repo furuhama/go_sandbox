26a2e340cddc98c908818dd13f7d3cfbd6680880
furuhama <furuhama@users.noreply.github.com>
2017-11-15 12:25:15 +0900

    rename src/mathapp/main.go to print_sqrt.go

---
+++ src/mathapp/print_sqrt.go
@@ -0,0 +1,11 @@
+package mathapp
+
+import (
+	"fmt"
+	"mymath"
+)
+
+// PrintSqrt prints sqrt of arg in stdout
+func PrintSqrt(x float64) {
+	fmt.Printf("Hello, world. Sqrt(%v) = %v\n", x, mymath.Sqrt(x))
+}
