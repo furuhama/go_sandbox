7e2fd3e8cfa48f5db4da4f35f80bfbd72c5ae09d
furuhama <furuhama@users.noreply.github.com>
2017-11-15 13:03:45 +0900

    make my_url_fetch.go

---
+++ src/tutorial/my_url_fetch.go
@@ -0,0 +1,27 @@
+package tutorial
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+// MyURLFetch parse url as args in command line
+// then, send http request to url
+func MyURLFetch() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
