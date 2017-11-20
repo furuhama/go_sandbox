cf1b25b32c7a459f6fcee1b7d6d0f9a16bd14dc3
furuhama <furuhama@users.noreply.github.com>
2017-11-20 16:36:45 +0900

    add response info

--- src/tutorial/my_server2.go
+++ src/tutorial/my_server2.go
@@ -18,6 +18,18 @@ func MyServer2() {
 
 // handler returns Path element of requested URL
 func handler2(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "Remote Addr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 	mu.Lock()
 	count++
 	mu.Unlock()
