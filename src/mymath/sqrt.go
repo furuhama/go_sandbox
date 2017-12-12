6a84e8323c9efc76662a6f8d48a9347adde1f4ca
furuhama <furuhama@users.noreply.github.com>
2017-12-12 12:23:34 +0900

    lint mymath/sqrt.go

--- src/mymath/sqrt.go
+++ src/mymath/sqrt.go
@@ -1,10 +1,12 @@
+// Package mymath is original implementation of math functions
 // Source code of $GOPATH/src/mymath/sqrt.go
 package mymath
 
+// Sqrt returns the square root
 func Sqrt(x float64) float64 {
-  z := 0.0
-  for i := 0; i < 1000; i++ {
-    z -= (z*z - x) / (2 * x)
-  }
-  return z
+	z := 0.0
+	for i := 0; i < 1000; i++ {
+		z -= (z*z - x) / (2 * x)
+	}
+	return z
 }
