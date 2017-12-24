f0d4a37f0822686ba0bfb2fce34aab2ae36f9cb9
furuhama <furuhama@users.noreply.github.com>
2017-12-24 23:37:09 +0900

    Add type Myfloat & its method Abs

---
+++ src/mymethod/myfloat.go
@@ -0,0 +1,12 @@
+package mymethod
+
+// MyFloat is original float64 type
+type MyFloat float64
+
+// Abs is for MyFloat type method
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		f = f * -1
+	}
+	return float64(f)
+}
