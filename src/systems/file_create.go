be656236ef335bbfa46e06d9c69184cc9ce0c3e3
furuhama <furuhama@users.noreply.github.com>
2018-02-10 09:35:11 +0900

    Add TCPConnect function

--- src/systems/file_create.go
+++ src/systems/file_create.go
@@ -4,6 +4,8 @@ package systems
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"net"
 	"os"
 )
 
@@ -27,4 +29,14 @@ func Buffer() {
 	var buffer bytes.Buffer
 	buffer.Write([]byte("bytes.Buffer example\n"))
 	fmt.Println(buffer.String())
+}
+
+// TCPConnect make connection by tcp & returns its result in Stdout
+func TCPConnect() {
+	conn, err := net.Dial("tcp", "furuhama.github.io:80")
+	if err != nil {
+		panic(err)
+	}
+	conn.Write([]byte("GET / HTTP/1.0\r\nHost: furuhama.github.io\r\n\r\n"))
+	io.Copy(os.Stdout, conn)
 }
