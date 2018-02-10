20f09c6e76f05f25b36538fe5b56ebf1e8dcf7b0
furuhama <furuhama@users.noreply.github.com>
2018-02-10 18:34:14 +0900

    Define HandleHTML()

--- src/systems/network.go
+++ src/systems/network.go
@@ -2,6 +2,8 @@
 package systems
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"net"
 	"net/http"
@@ -26,4 +28,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func Handling() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8000", nil)
+}
+
+// HandleHTML is extended `Handling()`, parse HTML
+func HandleHTML() {
+	conn, err := net.Dial("tcp", "furuhama.github.io:80")
+	if err != nil {
+		panic(err)
+	}
+	conn.Write([]byte("GET / HTTP/1.0\r\nHost: furuhama.github.io\r\n\r\n"))
+	res, err := http.ReadResponse(bufio.NewReader(conn), nil)
+	// print header info
+	fmt.Println(res.Header)
+	// print body info
+	defer res.Body.Close()
+	io.Copy(os.Stdout, res.Body)
 }
