d6175135408f4844d8e1fe6072ccac22703a6dd9
furuhama <furuhama@users.noreply.github.com>
2018-05-18 09:39:49 +0900

    Update main.go to use go routine & channel system

--- main.go
+++ main.go
@@ -2,11 +2,32 @@ package main
 
 import (
 	"fmt"
-	"systems"
+	"log"
+	// "systems"
 )
 
+// func main() {
+//     fmt.Println("TCPSocketClient start")
+//     systems.TCPSocketClientPipeline()
+//     fmt.Println("TCPSocketClient end")
+// }
+
+func worker(msg string) <-chan string {
+	receiver := make(chan string)
+	for i := 0; i < 3; i++ {
+		// go routine で無名関数を呼び出している
+		go func(i int) {
+			// ここで msg に文字列を入れて、そのあと receiver chan から返している
+			msg := fmt.Sprintf("%d %s done", i, msg)
+			receiver <- msg
+		}(i)
+	}
+	return receiver
+}
+
 func main() {
-	fmt.Println("TCPSocketClient start")
-	systems.TCPSocketClientPipeline()
-	fmt.Println("TCPSocketClient end")
+	receiver := worker("job")
+	for i := 0; i < 3; i++ {
+		log.Println(<-receiver)
+	}
 }
