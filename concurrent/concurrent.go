360b156fb94e2eefaf559b1160ef5801ab7eb116
furuhama <furuhama@users.noreply.github.com>
2018-07-21 19:20:42 +0900

    Move testing goroutine functions to concurrent package

---
+++ concurrent/concurrent.go
@@ -0,0 +1,27 @@
+package concurrent
+
+import (
+	"fmt"
+	"log"
+)
+
+func worker(msg string) <-chan string {
+	receiver := make(chan string)
+	for i := 0; i < 3; i++ {
+		// go routine で無名関数を呼び出している
+		go func(i int) {
+			// ここで msg に文字列を入れて、そのあと receiver chan から返している
+			msg := fmt.Sprintf("%d %s done", i, msg)
+			receiver <- msg
+		}(i)
+	}
+	return receiver
+}
+
+// RunWorker は worker を走らせるテスト
+func RunWorker() {
+	receiver := worker("job")
+	for i := 0; i < 3; i++ {
+		log.Println(<-receiver)
+	}
+}
--- main.go
+++ main.go
@@ -1,26 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"github.com/furuhama/go_sandbox/concurrent"
 )
 
-func worker(msg string) <-chan string {
-	receiver := make(chan string)
-	for i := 0; i < 3; i++ {
-		// go routine で無名関数を呼び出している
-		go func(i int) {
-			// ここで msg に文字列を入れて、そのあと receiver chan から返している
-			msg := fmt.Sprintf("%d %s done", i, msg)
-			receiver <- msg
-		}(i)
-	}
-	return receiver
-}
-
 func main() {
-	receiver := worker("job")
-	for i := 0; i < 3; i++ {
-		log.Println(<-receiver)
-	}
+	concurrent.RunWorker()
 }
